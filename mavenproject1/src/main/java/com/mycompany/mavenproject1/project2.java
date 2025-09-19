7d9508ca5237c6862696af25c08f1aab502b48a9
CL2~PC21 <CL2~PC21@DESKTOP-T0JRCSN>
2025-09-19 17:44:35 +0800

    sanaokay na

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/project2.java
@@ -0,0 +1,27 @@
+/*import java.util.HashSet;
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+import java.util.HashSet;
+
+/**
+ *
+ * @author CL2~PC21
+ */
+public class project2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+    HashSet<String> cars = new HashSet<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("BMW");  // Duplicate
+    cars.add("Mazda");
+    System.out.println(cars);
+    }
+}
