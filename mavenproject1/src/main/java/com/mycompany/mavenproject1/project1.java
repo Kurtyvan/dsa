4631bb4568f207556a125b6a50fcdf8161daf3c2
CL2~PC21 <CL2~PC21@DESKTOP-T0JRCSN>
2025-09-19 17:43:26 +0800

    sanaokay na

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/project1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author CL2~PC21
+ */
+import java.util.ArrayList;
+public class project1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+    ArrayList<String> cars = new ArrayList<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+    cars.add("rusi");
+    System.out.println(cars);
+
+    }    
+}
+
