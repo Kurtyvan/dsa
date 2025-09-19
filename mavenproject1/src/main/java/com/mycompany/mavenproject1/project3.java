11a0ce3e88e79bb7bcfe7282059e0ea212c98f4d
CL2~PC21 <CL2~PC21@DESKTOP-T0JRCSN>
2025-09-19 17:45:04 +0800

    sanaokay na

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/project3.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author CL2~PC21
+ */
+import java.util.HashMap;
+public class project3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+    HashMap<String, String> capitalCities = new HashMap<String, String>();
+
+    // Add keys and values (Country, City)
+    capitalCities.put("England", "London");
+    capitalCities.put("Germany", "Berlin");
+    capitalCities.put("Norway", "Oslo");
+    capitalCities.put("USA", "Washington DC");
+    System.out.println(capitalCities);
+   }
+}
+   
